e360c01d802c2fb92fefdd59de461e04c45b6ea1
Ada <witamborska.ada@gmail.com>
2024-11-17 09:19:36 +0100

    feat: add restuarant model

---
+++ app/src/main/java/com/dsw/pam/vegebite/domain/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.dsw.pam.vegebite.domain
+
+data class Restaurant(
+    val id: Int,
+    val name: String,
+    val address: String,
+    val image: String,
+    val description: String,
+)
